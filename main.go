f12ea0c2b4a30d4ec6b725fb95b55f4a10ccc968
Lukas Möller <lukasmoeller@urlukas.com>
2021-10-17 13:24:34 +0200

    main.go: add twelve tries for the user

--- main.go
+++ main.go
@@ -10,8 +10,17 @@ func main() {
 	code := []rune{49, 50, 51, 52}
 
 	fmt.Println("🤖 Please give the machine some words/characters to work with:")
-	ui := get.UsrInpt()
-	fmt.Println(ui)
 
-	fmt.Println(get.Fdbk(code, ui))
+	for i := 0; i < 12; i++ {
+		ui := get.UsrInpt()
+		fmt.Println(ui)
+
+		fdbk := get.Fdbk(code, ui)
+		fmt.Println(fdbk)
+
+		if get.Compare(code, ui) {
+			fmt.Println("CRACKED 🔓")
+			os.Exit(0)
+		}
+	}
 }
